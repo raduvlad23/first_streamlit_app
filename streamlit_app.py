2ec02dbe4edbba32eb58e2da3239b964be71c241
raduvlad23 <123649199+raduvlad23@users.noreply.github.com>
2023-02-07 14:19:47 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title("My Parents Diner 2")
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
