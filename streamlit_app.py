c80266a3cdc208daff286873c487e7603e26ef17
raduvlad23 <123649199+raduvlad23@users.noreply.github.com>
2023-02-07 14:28:01 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,2 @@
 import pandas;
-pip install streamlit;
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt");
-streamlit.dataframe(my_fruit_list);
